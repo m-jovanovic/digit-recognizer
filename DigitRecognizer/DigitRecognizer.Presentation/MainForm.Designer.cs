10262e7e0fdd632f989406d8db085bf131f1b891
thelanmi <milannis.995@gmail.com>
2018-07-30 13:48:58 +1100

    Implemented drawing on the form in the presentation project

--- DigitRecognizer/DigitRecognizer.Presentation/MainForm.Designer.cs
+++ DigitRecognizer/DigitRecognizer.Presentation/MainForm.Designer.cs
@@ -28,22 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.drawingPanel = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDecide = new System.Windows.Forms.Button();
+            this.lblDecision = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // drawingPanel
+            // 
+            this.drawingPanel.BackColor = System.Drawing.Color.White;
+            this.drawingPanel.Location = new System.Drawing.Point(43, 30);
+            this.drawingPanel.Name = "drawingPanel";
+            this.drawingPanel.Size = new System.Drawing.Size(400, 400);
+            this.drawingPanel.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.White;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(368, 474);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 30);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnDecide
+            // 
+            this.btnDecide.BackColor = System.Drawing.Color.White;
+            this.btnDecide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDecide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecide.Location = new System.Drawing.Point(43, 474);
+            this.btnDecide.Name = "btnDecide";
+            this.btnDecide.Size = new System.Drawing.Size(75, 30);
+            this.btnDecide.TabIndex = 2;
+            this.btnDecide.Text = "Decide";
+            this.btnDecide.UseVisualStyleBackColor = false;
+            // 
+            // lblDecision
+            // 
+            this.lblDecision.AutoSize = true;
+            this.lblDecision.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDecision.ForeColor = System.Drawing.Color.Black;
+            this.lblDecision.Location = new System.Drawing.Point(40, 439);
+            this.lblDecision.Name = "lblDecision";
+            this.lblDecision.Size = new System.Drawing.Size(66, 24);
+            this.lblDecision.TabIndex = 3;
+            this.lblDecision.Text = "label1";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.WindowText;
-            this.ClientSize = new System.Drawing.Size(484, 511);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(484, 521);
+            this.Controls.Add(this.lblDecision);
+            this.Controls.Add(this.btnDecide);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.drawingPanel);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Digit Recognizer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel drawingPanel;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDecide;
+        private System.Windows.Forms.Label lblDecision;
     }
 }
 
--- DigitRecognizer/DigitRecognizer.Presentation/MainForm.cs
+++ DigitRecognizer/DigitRecognizer.Presentation/MainForm.cs
@@ -1,12 +1,108 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
 
 namespace DigitRecognizer.Presentation
 {
     public partial class MainForm : Form
     {
+        private Point _currentPoint;
+        private Point _previousPoint;
+        private readonly Pen _pen = new Pen(Color.Black, 55);
+        private readonly Bitmap _bitmap;
+
         public MainForm()
         {
             InitializeComponent();
+            _bitmap = new Bitmap(drawingPanel.Width, drawingPanel.Height);
+            using (Graphics g = Graphics.FromImage(_bitmap))
+            {
+                g.Clear(Color.White);
+            }
+            _pen.SetLineCap(LineCap.Round, LineCap.Round, DashCap.Round);
+
+            drawingPanel.MouseDown += DrawingPanel_MouseDown;
+            drawingPanel.MouseMove += DrawingPanel_MouseMove;
+            drawingPanel.Paint += DrawingPanel_Paint;
+
+            btnClear.Click += BtnClear_Click;
+            btnDecide.Click += BtnDecide_Click;
+
+            lblDecision.Text = string.Empty;
+        }
+
+        private void DrawingPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            _previousPoint = e.Location;
+        }
+
+        private void DrawingPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _currentPoint = e.Location;
+                const int retryCount = 5;
+                for (var i = 0; i < retryCount; i++)
+                {
+                    try
+                    {
+                        using (Graphics g = Graphics.FromImage(_bitmap))
+                        {
+                            g.DrawLine(_pen, _previousPoint, _currentPoint);
+                        }
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                    break;
+                }
+                drawingPanel.Invalidate();
+                _previousPoint = _currentPoint;
+            }
+        }
+
+        private void DrawingPanel_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(_bitmap, Point.Empty);
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            const int retryCount = 5;
+            for (var i = 0; i < retryCount; i++)
+            {
+                try
+                {
+                    using (Graphics g = Graphics.FromImage(_bitmap))
+                    {
+                        g.Clear(Color.White);
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+
+                break;
+            }
+            drawingPanel.Invalidate();
+            lblDecision.Text = string.Empty;
+        }
+        
+        private void BtnDecide_Click(object sender, EventArgs e)
+        {
+            //double[] pixels = _bitmap.Preprocess();
+            //var prediction = nn.Decide(pixels);
+            //var argMax = prediction.ToList().IndexOf(prediction.Max());
+            lblDecision.Text = "Hello thereeeeeeeeeeeeee"; //$@"{argMax} ({prediction[argMax]:P2})";
         }
     }
 }
